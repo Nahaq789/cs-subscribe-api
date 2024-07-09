b6e463c2690578af1177085ccd13e74e16570d85
Nahaq <naha022677@icloud.com>
2024-07-09 21:46:28 +0000

    パラメータなしコンストラクタ

--- src/Subscribe.Domain/Model/SubscribeAggregate/SubscribeItem.cs
+++ src/Subscribe.Domain/Model/SubscribeAggregate/SubscribeItem.cs
@@ -37,6 +37,10 @@ public class SubscribeItem
         SubscribeAggregateId = subscribeAggregateId;
     }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    public SubscribeItem() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
     /// <summary>
     /// サブスクリプションの金額を設定します。
     /// </summary>
