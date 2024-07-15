480fd8e0566dbbeac4bf03b204d1a94b04bb09b2
Ran Mouri <naha022677@icloud.com>
2024-07-15 12:57:08 +0900

    feat: get subscribe by userId

--- src/Subscribe.API/Presentation/SubscribeController.cs
+++ src/Subscribe.API/Presentation/SubscribeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Subscribe.API.Application.Command;
 using Subscribe.API.Application.Command.Subscribe;
+using Subscribe.API.Application.Query.Subscribe;
 
 namespace Subscribe.API.Presentation;
 
@@ -12,11 +13,13 @@ public class SubscribeController : Controller
 {
     private readonly IMediator _mediator;
     private readonly ILogger<SubscribeController> _logger;
+    private readonly ISubscribeQueries _subscribeQueries;
 
-    public SubscribeController(IMediator mediator, ILogger<SubscribeController> logger)
+    public SubscribeController(IMediator mediator, ILogger<SubscribeController> logger, ISubscribeQueries subscribeQueries)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _subscribeQueries = subscribeQueries ?? throw  new ArgumentNullException(nameof(subscribeQueries));
     }
 
     [HttpPost("create")]
@@ -136,5 +139,21 @@ public class SubscribeController : Controller
             _logger.LogError("Error Message: {}", ex.Message);
             return TypedResults.Problem(detail: ex.Message, statusCode: 500);
         }
+    }
+
+    [HttpGet]
+    [Route("/api/v1/subscribe/find")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<Results<Ok<IEnumerable<SubscribeAggregateDto>>, BadRequest<string>>> GetSubscribeByUserAsync(
+        [FromHeader(Name = "x-requestId")] Guid requestId, [FromQuery] Guid userid)
+    {
+        if (requestId == Guid.Empty)
+        {
+            _logger.LogWarning("RequestId is empty");
+            return TypedResults.BadRequest("RequestId is empty");
+        }
+        var result = await _subscribeQueries.GetSubscribeByUserAsync(userid);
+        return TypedResults.Ok(result);
     }
 }
