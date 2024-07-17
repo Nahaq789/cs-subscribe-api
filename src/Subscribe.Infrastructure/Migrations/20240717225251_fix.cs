858f21c93594528aabcc181c6d4bd3ce71ceee01
Ran Mouri <naha022677@icloud.com>
2024-07-18 07:56:35 +0900

    fix: カテゴリIDが1対1の関係になっていたため修正

---
+++ src/Subscribe.Infrastructure/Migrations/20240717225251_fix.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Subscribe.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_subscribe_aggregate_category_aggregate_id",
+                table: "subscribe_aggregate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_subscribe_aggregate_category_aggregate_id",
+                table: "subscribe_aggregate",
+                column: "category_aggregate_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_subscribe_aggregate_category_aggregate_id",
+                table: "subscribe_aggregate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_subscribe_aggregate_category_aggregate_id",
+                table: "subscribe_aggregate",
+                column: "category_aggregate_id",
+                unique: true);
+        }
+    }
+}
